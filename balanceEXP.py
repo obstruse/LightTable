3311641313de1d037b5030af61c004be34b90ffe
pi <pi@sdr.home>
2023-03-14 17:06:56 -0700

    camera preview on TFT

--- balanceEXP.py
+++ balanceEXP.py
@@ -17,7 +17,7 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # read config file
 config = ConfigParser()
 config.read('config.ini')
-shutter = config.getint('PiCamera','shutter',fallback=30)
+#shutter = config.getint('PiCamera','shutter',fallback=30)
 iso     = config.getint('PiCamera','iso',fallback=200)
 
 # setup buttons
@@ -29,7 +29,7 @@ GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # TFT backlight
-GPIO.setwarnings(False)     # want to leave #18 OUTPUT and set to zero at end of program. generates warning
+GPIO.setwarnings(False)     # want to leave #18 OUTPUT and set to zero at end of program. It generates warning...
 GPIO.setup(18, GPIO.OUT)
 
 def tftOn():
@@ -47,12 +47,9 @@ GPIO.add_event_detect(22, GPIO.FALLING, callback=callback, bouncetime=300)
 GPIO.add_event_detect(23, GPIO.FALLING, callback=callback, bouncetime=300)
 GPIO.add_event_detect(27, GPIO.FALLING, callback=callback, bouncetime=300)
 
-# initialize display environment
-# Note that we don't instantiate any display!
-###pygame.init()
+# initialize pygame display environment
 pygame.font.init()
 pygame.display.init()
-#pygame.display.set_caption('Camera')
 font = pygame.font.SysFont(None,30)
 
 WHITE = (255,255,255)
@@ -63,34 +60,28 @@ timer = pygame.time.Clock()
 # initialize camera
 from picamera import PiCamera
 #cameraRes = (4056,3040)
-cameraRes = (2048,1520)
+#cameraRes = (2048,1520)
+cameraRes = (320,240)
 #cameraRes = (640,480)
 camera = PiCamera(sensor_mode=0,resolution=cameraRes)
-cameraRes = camera.resolution
-#shutter = 10
-#if shutter > 30 :
-#    camera.framerate = 20
-#else:
-#    camera.framerate = shutter
-#camera.shutter_speed = int (1000000/shutter)
-#camera.exposure_mode = 'off'
-camera.framerate_range = (1,40)    # low rate (1 FPS) is longest exposure allowed. 
+cameraRes = camera.resolution       # the actual resolution may not be the requested resolution
+print(f"camera res: {cameraRes}\n")
+cameraBuffer = bytearray(3*cameraRes[0]*cameraRes[1])
+
+camera.framerate_range = (10,30)    # preview minimum is 10 FPS.
+#camera.framerate = 1
 camera.iso           = iso
 camera.awb_mode      = 'auto'
 camera.exposure_mode = 'auto'
-#camera.framerate     = 10
-camera.annotate_text_size = 64
+camera.annotate_text_size = 20
 camera.annotate_background = True
 
-#camera.awb_mode = 'tungsten'
-#camera.annotate_text_size = 64
-#camera.annotate_background = True
+print(f"framerate {camera.framerate}\n")
+print(f"framerate_range {camera.framerate_range}\n")
 
 print("cam initialized")
 
-# initialize display surface
-# The pygame surface we are going to draw onto. 
-# /!\ It must be the exact same size of the target display /!\
+# initialize display surface for use on TFT
 lcd = pygame.Surface((320,240))
 #lcd = pygame.display.set_mode((640,480),pygame.FULLSCREEN)      # pixel dimension of LCD is 320,240, OS configured to 640,480
 lcdRes = lcd.get_size()
@@ -140,15 +131,16 @@ def displayUpdate():
     f.write(lcd.convert(16,0).get_buffer())
     # We can then close our access to the framebuffer
     f.close()
-    time.sleep(0.1)
+    # why is there a wait here..v
+    #time.sleep(0.1)
 
 zoom = False
 zoomLevel = lcdRes[0]/cameraRes[0]/2        # ratio of LCD : camera (size of zoom box)
 zoomX = zoomY = (1-zoomLevel)/2             # zoom box in middle
 
-lcd.fill((0,0,0))
-displayUpdate()
-camera.start_preview()
+##lcd.fill((0,0,0))
+##displayUpdate()
+###camera.start_preview()
 tftOn()
 
 active = True
@@ -196,18 +188,18 @@ while active:
                 print(f"time: {time.time() - timeStart}")
 
             # shutter speed (the keyboard I want to use doesn't have a key pad!!... so KP7-4-1 == T-G-B and KP8-5-2 == Y-H-N 
-            if e.key == K_KP7 or e.key == K_KP1 or e.key == K_t or e.key == K_b:
-                if e.key == K_KP7 or e.key == K_t:
-                    shutter += 1
-                else :
-                    shutter -= 1
-                if shutter < 1 :
-                    shutter = 1
-                if shutter > 20 :
-                    camera.framerate = 20
-                else:
-                    camera.framerate = shutter
-                camera.shutter_speed = int (1000000/shutter)
+            #if e.key == K_KP7 or e.key == K_KP1 or e.key == K_t or e.key == K_b:
+            #    if e.key == K_KP7 or e.key == K_t:
+            #        shutter += 1
+            #    else :
+            #        shutter -= 1
+            #    if shutter < 1 :
+            #        shutter = 1
+            #    if shutter > 20 :
+            #        camera.framerate = 20
+            #    else:
+            #        camera.framerate = shutter
+            #    camera.shutter_speed = int (1000000/shutter)
 
             # save settings
             if e.key == K_KP4 or e.key == K_g:
@@ -215,7 +207,10 @@ while active:
                 with open('config.ini', 'w') as f:
                     config.write(f)
 
-    lcd.fill(BLACK)
+    ##lcd.fill(BLACK)
+    camera.capture(cameraBuffer, format='rgb')
+    cameraImage = pygame.image.frombuffer(cameraBuffer,cameraRes, 'RGB')
+    lcd.blit(cameraImage,(0,0))
 
     EXPnum = font.render('%d'%camera.exposure_speed, True, WHITE)
     textPos = EXPnum.get_rect(center=EXPnumPos.center)
