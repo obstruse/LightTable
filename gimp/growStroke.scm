633ed2dcf12a5f2e1a7cf014a10ae593193e84ca
neisius <neisius@earthlink.net>
2023-03-26 10:38:09 -0700

    workflow

---
+++ gimp/growStroke.scm
@@ -0,0 +1,55 @@
+(define (script-fu-grow-stroke inImage inDrawable inGrowth inColor inLoop inWidth inDirname) 
+    (let* (
+           (theImage inImage)
+           (theDrawable inDrawable)
+           (theGrowth inGrowth)
+           (theColor inColor)
+           (theLoop inLoop)
+           (theWidth inWidth)
+           (theDirname inDirname)
+           (fileName "")
+           )
+      (gimp-image-undo-group-start theImage)
+
+      (gimp-context-set-foreground theColor)
+      (gimp-context-set-line-width theWidth)
+      (gimp-context-set-stroke-method 0)
+      (gimp-context-set-antialias FALSE)
+
+      (while (> theLoop 0)
+          ;(gimp-message (number->string theLoop))
+          ;(gimp-context-set-line-width theLoop)
+          ;(gimp-message (number->string(car (gimp-context-get-line-width))))
+          (gimp-selection-grow theImage theGrowth)
+          (gimp-drawable-edit-stroke-selection theDrawable)
+          (set! fileName (string-append theDirname "/growth-" (number->string ( + 100 theLoop)) ".png" ))
+          ;(file-png-save 1 theImage theDrawable fileName fileName 0 9 0 0 0 0 0)
+          (set! theLoop (- theLoop 1))
+      )
+
+      (gimp-displays-flush)
+      (gimp-image-undo-group-end theImage)
+
+  )
+)
+
+; Register the function with GIMP:
+
+(script-fu-register "script-fu-grow-stroke"
+  "Grow Stroke..."
+  "Grow the selection and stroke it"
+  "Bill Neisius"
+  ""
+  "24 Jan 2023"
+  ""
+  SF-IMAGE       "The image"   0
+  SF-DRAWABLE    "The layer"   0
+  SF-ADJUSTMENT  "Growth"      '(20 1 100 1 1 0 0)
+  SF-COLOR       "Color"       '(255 255 255)
+  SF-ADJUSTMENT  "Loop"        '(1 1 100 1 1 0 0)
+  SF-ADJUSTMENT  "Width"       '(1 1 100 1 1 0 0)
+  SF-DIRNAME     "Directory for save" "./"
+)
+
+(script-fu-menu-register "script-fu-grow-stroke" "<Image>/Filters/Script-Fu")
+
